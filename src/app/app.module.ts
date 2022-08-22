a4b04500098ef92f8bc44504ca4be213ae1eb80b
Ilaria <idellaquila@sysmanagement.it>
2022-08-22 10:22:49 +0200

    creazione componente card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreditCardComponent } from './shared/components/credit-card/credit-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreditCardComponent
   ],
   imports: [
     BrowserModule,
