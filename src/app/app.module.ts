dfe2989c0095569e3da44e1020978b1f0f27ca54
Ilaria <idellaquila@sysmanagement.it>
2022-08-22 11:26:38 +0200

    Continuo card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
-import { CreditCardComponent } from './shared/components/credit-card/credit-card.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    CreditCardComponent
+
   ],
   imports: [
     BrowserModule,
